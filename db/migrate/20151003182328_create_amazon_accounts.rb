a7e3a5207bed36ab9c458f4b96dbf949d190772c
Nafaa Boutefer <nafaabout@gmail.com>
2015-10-07 11:26:19 +0100

    Change amazon_account fields names.

--- db/migrate/20151003182328_create_amazon_accounts.rb
+++ db/migrate/20151003182328_create_amazon_accounts.rb
@@ -2,9 +2,9 @@ class CreateAmazonAccounts < ActiveRecord::Migration
   def change
     create_table :amazon_accounts do |t|
       t.belongs_to :shop, index: true, foreign_key: true
-      t.string :seller_id
+      t.string :merchant_id
       t.string :marketplace_id
-      t.string :mws_auth_token
+      t.string :auth_token
 
       t.timestamps null: false
     end
